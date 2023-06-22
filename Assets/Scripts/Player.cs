e082f7e3d0b81cb21b401c47d4a0c3b7a760de5f
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-06-21 21:11:43 -0300

    Implemented Player Move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Ships
+{
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            PlayerMove();
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    private void PlayerMove()
+    {
+        Vector2 direction;
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+        Move(direction);
+    }
+}
