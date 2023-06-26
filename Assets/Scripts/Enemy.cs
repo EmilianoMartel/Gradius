5defeca7c63631d6e9e3caadfda023e0436032a1
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-06-26 17:51:08 -0300

    Implemented Enemy PatternMovement Logic

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,9 +11,21 @@ public class Enemy : Ships
         Up,
         Down
     }
+    private enum PatternMovement
+    {
+        UpDown,
+        DownUp,
+        LineUp,
+        LineDown
+    }
 
-    [SerializeField] private MovementType m_MovementType;
+    //variables patron movimiento
+    private MovementType m_MovementType;
+    [SerializeField] private PatternMovement m_PatternMovement;
     private Vector2 m_movement = new Vector2(0,0);
+    private bool lineType;
+    [SerializeField] private float m_timeChange; //solo para los lineType
+    private float actualTimeChange = 0;
 
     //variables para los disparos
     private float m_timeShoot;
@@ -22,20 +34,33 @@ public class Enemy : Ships
     void Start()
     {
         CameraLimit();
-        MoveTypeSelection();
+        MovePattern();
         TimeShootSelection();
     }
 
     // Update is called once per frame
     void Update()
     {
-        actualTime += Time.deltaTime;
+        actualTime += Time.deltaTime;        
         Move(m_movement);
         if(actualTime >= m_timeShoot)
         {
             Shoot();
             TimeShootSelection();
             actualTime= 0;
+        }
+        if (lineType)
+        {
+            actualTimeChange += Time.deltaTime;
+            if (actualTimeChange >= m_timeChange)
+            {
+                LineTypeChange();
+                lineType = false;
+            }
+        }
+        else
+        {
+            DiagonalTypeChange();
         }
     }
 
@@ -58,9 +83,62 @@ public class Enemy : Ships
         }
     }
 
-    private void EnemyMove()
+    private void MovePattern()
     {
+        switch (m_PatternMovement)
+        {
+            case PatternMovement.UpDown:
+                m_MovementType = MovementType.Up;
+                lineType = false;
+                MoveTypeSelection();
+                break;
+            case PatternMovement.DownUp:
+                m_MovementType = MovementType.Down;
+                lineType = false;
+                MoveTypeSelection();
+                break;
+            case PatternMovement.LineUp:
+                m_MovementType = MovementType.Line;
+                lineType = true;
+                MoveTypeSelection();
+                break;
+            case PatternMovement.LineDown:
+                m_MovementType = MovementType.Line;
+                lineType = true;
+                MoveTypeSelection();
+                break;
+        }
+        Debug.Log(m_MovementType);
+        Debug.Log(m_PatternMovement);
+    }
 
+    private void LineTypeChange()
+    {
+        if (m_PatternMovement == PatternMovement.LineUp)
+        {
+            m_MovementType = MovementType.Up;
+            MoveTypeSelection();
+        }
+        else
+        {
+            m_MovementType = MovementType.Down;
+            MoveTypeSelection();
+        }
+    }
+    private void DiagonalTypeChange()
+    {
+        Debug.Log(upperLimit);
+        Debug.Log(lowerLimit);
+        if(transform.position.y >= upperLimit)
+        {
+            m_MovementType = MovementType.Down;
+            MoveTypeSelection();
+        }
+        if(transform.position.y <= lowerLimit)
+        {
+            m_MovementType = MovementType.Up;
+            MoveTypeSelection();
+        }
     }
 
     private void TimeShootSelection()
