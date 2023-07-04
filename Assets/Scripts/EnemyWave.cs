b0bd0c4fa55e2937af2ead831cdad9d2bc4c2fcc
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-07-04 18:35:20 -0300

    Update Script Player Enemy

--- Assets/Scripts/EnemyWave.cs
+++ Assets/Scripts/EnemyWave.cs
@@ -6,19 +6,38 @@ public class EnemyWave : MonoBehaviour
 {
     [SerializeField] WaveData waveData;
     [SerializeField] Vector3 p_position;
+    [SerializeField] float enemyNum;
+    private int currentWave;
+    private bool _activateWave = false;
+    private List<Enemy> _enemyList = new List<Enemy>();
 
     private void Start()
     {
-        p_position = transform.position;
+        currentWave = 0;
+        MaxEnemyGenerator();
         StartCoroutine(Waves());
+    }
+    private void Update()
+    {
+        
     }
 
     private IEnumerator Waves()
     {
-        for (int i = 0; i < waveData.enemyList.Count; i++)
+        for (int i = 0; i < enemyNum; i++)
         {
-            Instantiate(waveData.enemyList[i], p_position, Quaternion.identity);
+            waveData.enemyList.Add(Instantiate(waveData.enemyList[0], transform.position, Quaternion.identity));
             yield return new WaitForSeconds(0.5f);
         }
+    }
+
+    private void MaxEnemyGenerator()
+    {
+        enemyNum = Random.Range(waveData.minEnemy,waveData.enemyList.Count);
+    }
+
+    private void ChangeWave()
+    {
+
     }
 }
