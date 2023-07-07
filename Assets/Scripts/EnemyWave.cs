426fff9bddb3e010fa324118ef710963f0c56585
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-07-06 21:14:50 -0300

    Update EnemyWaves

--- Assets/Scripts/EnemyWave.cs
+++ Assets/Scripts/EnemyWave.cs
@@ -11,33 +11,77 @@ public class EnemyWave : MonoBehaviour
     private bool _activateWave = false;
     private List<Enemy> _enemyList = new List<Enemy>();
 
+    //variables spawner
+    [SerializeField] GameObject spawnTowerUp;
+    [SerializeField] GameObject spawnTowerDown;
+    [SerializeField] GameObject spawnShipUp;
+    [SerializeField] GameObject spawnShipDown;
+
+    //variables WaveElection
+    private int waveCount;
+
     private void Start()
     {
         currentWave = 0;
-        MaxEnemyGenerator();
-        StartCoroutine(Waves());
-    }
-    private void Update()
-    {
-        
+        RandomGenerationSpawn();
     }
 
-    private IEnumerator Waves()
+    private void Update()
     {
-        for (int i = 0; i < enemyNum; i++)
+
+    }
+
+    private IEnumerator WavesShip()
+    {
+        for (int i = 0; i <= enemyNum; i++)
         {
-            waveData.enemyList.Add(Instantiate(waveData.enemyList[0], transform.position, Quaternion.identity));
+            _enemyList.Add(Instantiate(waveData.ship, transform.position, Quaternion.identity));
             yield return new WaitForSeconds(0.5f);
         }
     }
 
+    private void TowerSpawn()
+    {
+        Instantiate(waveData.tower,transform.position,Quaternion.identity);
+    }
+
     private void MaxEnemyGenerator()
     {
-        enemyNum = Random.Range(waveData.minEnemy,waveData.enemyList.Count);
+        enemyNum = Random.Range(waveData.minEnemy,waveData.maxEnemy);
     }
 
     private void ChangeWave()
     {
 
+    }
+
+    private void CallSpawn()
+    {
+        int wave;
+        for (int i = 0; i <= waveCount; i++)
+        {
+            wave = Random.Range(0, 1);
+            if (wave == 0)
+            {
+                MaxEnemyGenerator();
+                StartCoroutine(WavesShip());
+            } else if (wave == 1)
+            {
+                TowerSpawn();
+            }
+        }
+    }
+
+    private void RandomGenerationSpawn()
+    {
+        if (currentWave <= 3)
+        {
+            waveCount = Random.Range(1,3);
+        }
+        else
+        {
+            waveCount = Random.Range(2, currentWave);
+        }
+        CallSpawn();
     }
 }
